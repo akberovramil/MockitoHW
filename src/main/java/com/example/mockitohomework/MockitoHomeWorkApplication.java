6999fdbcddf86532d159fa64e84915e373ebcaf5
akberovramil <ramil.akberov96@gmail.com>
2023-03-09 16:02:31 +0300

    Commit

---
+++ src/main/java/com/example/mockitohomework/MockitoHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockitohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoHomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockitohomework/MockitoHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockitohomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
